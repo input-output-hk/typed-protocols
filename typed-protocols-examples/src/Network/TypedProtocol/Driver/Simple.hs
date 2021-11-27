ff184d72782765756d243c20cb67f04ea22dbb6c
Marcin Szamotulski <profunctor@pm.me>
2021-11-27 10:50:30 +0100

    CollectSTM: simpleDriver in typed-protocols-examples

--- typed-protocols-examples/src/Network/TypedProtocol/Driver/Simple.hs
+++ typed-protocols-examples/src/Network/TypedProtocol/Driver/Simple.hs
@@ -39,8 +39,11 @@ import           Network.TypedProtocol.Core
 import           Network.TypedProtocol.Driver
 import           Network.TypedProtocol.Peer
 
+import           Control.Applicative ((<|>))
+import           Control.Exception (SomeAsyncException (..))
 import           Control.Monad.Class.MonadAsync
-import           Control.Monad.Class.MonadSTM
+import           Control.Monad.Class.MonadFork
+import           Control.Monad.Class.MonadSTM.Strict
 import           Control.Monad.Class.MonadThrow
 import           Control.Tracer (Tracer (..), contramap, traceWith)
 
@@ -81,14 +84,40 @@ instance Show (AnyMessage ps) => Show (TraceSendRecv ps) where
   show (TraceRecvMsg msg) = "Recv " ++ show msg
 
 
+-- | An existential handle to an 'async' thread.
+--
+data SomeAsync m where
+    SomeAsync :: forall m a. !(Async m a) -> SomeAsync m
+
+-- | A simple driver.
+--
+-- It is not pure, because it exposes access to the thread which is started by
+-- 'recvMessageSTM'.  This is useful for proper handling of asynchronous
+-- exceptions.  There can be at most one such thread at a time.
+--
 driverSimple :: forall ps (pr :: PeerRole) failure bytes m.
-                (MonadThrow m, Exception failure)
+                ( MonadAsync      m
+                , MonadMask       m
+                , MonadThrow (STM m)
+                , Exception failure
+                )
              => Tracer m (TraceSendRecv ps)
              -> Codec ps failure m bytes
              -> Channel m bytes
-             -> Driver ps pr bytes failure (Maybe bytes)  m
-driverSimple tracer Codec{encode, decode} channel@Channel{send} =
-    Driver { sendMessage, recvMessage, tryRecvMessage, startDState = Nothing }
+             -> m ( Driver ps pr bytes failure (Maybe bytes) m
+                  , StrictTVar m (Maybe (SomeAsync m))
+                  )
+driverSimple tracer Codec{encode, decode} channel@Channel{send} = do
+    v <- newTVarIO Nothing
+    return
+      ( Driver { sendMessage
+               , recvMessage
+               , tryRecvMessage
+               , recvMessageSTM = recvMessageSTM v
+               , startDState = Nothing
+               }
+      , v
+      )
   where
     sendMessage :: forall (st :: ps) (st' :: ps).
                    ( SingI st
@@ -110,17 +139,16 @@ driverSimple tracer Codec{encode, decode} channel@Channel{send} =
                 => (ReflRelativeAgency (StateAgency st)
                                         TheyHaveAgency
                                        (Relative pr (StateAgency st)))
-                -> Either ( DecodeStep bytes failure m (SomeMessage st)
-                          , Maybe bytes
-                          )
-                          (Maybe bytes)
+                -> DriverState ps pr st bytes failure (Maybe bytes) m
                 -> m (SomeMessage st, Maybe bytes)
     recvMessage _ state = do
       result  <- case state of
-        Left (decoder, trailing) ->
+        DecoderState decoder trailing ->
           runDecoderWithChannel channel trailing decoder
-        Right trailing ->
+        DriverState trailing ->
           runDecoderWithChannel channel trailing =<< decode sing
+        DriverStateSTM stmRecvMessage _trailing ->
+          Right <$> atomically stmRecvMessage
       case result of
         Right x@(SomeMessage msg, _trailing') -> do
           traceWith tracer (TraceRecvMsg (AnyMessage msg))
@@ -135,29 +163,67 @@ driverSimple tracer Codec{encode, decode} channel@Channel{send} =
                    => (ReflRelativeAgency (StateAgency st)
                                            TheyHaveAgency
                                           (Relative pr (StateAgency st)))
-                   -> Either ( DecodeStep bytes failure m (SomeMessage st)
-                             , Maybe bytes
-                             )
-                             (Maybe bytes)
-                   -> m (Either ( DecodeStep bytes failure m (SomeMessage st)
-                                , Maybe bytes
-                                )
+                   -> DriverState ps pr st bytes failure (Maybe bytes) m
+                   -> m (Either (DriverState ps pr st bytes failure (Maybe bytes) m)
                                 (SomeMessage st, Maybe bytes))
     tryRecvMessage _ state = do
-      result <-
-        case state of
-          Left (decoder, trailing) ->
-            tryRunDecoderWithChannel channel trailing decoder
-          Right trailing ->
-            tryRunDecoderWithChannel channel trailing =<< decode sing
-      case result of
-        Right x@(Right (SomeMessage msg, _trailing')) -> do
-          traceWith tracer (TraceRecvMsg (AnyMessage msg))
-          return x
-        Right x@Left {} ->
-          return x
-        Left failure ->
-          throwIO failure
+        result <-
+          case state of
+            DecoderState decoder trailing ->
+              tryRunDecoderWithChannel channel trailing decoder
+            DriverState trailing ->
+              tryRunDecoderWithChannel channel trailing =<< decode sing
+            DriverStateSTM stmRecvMessage _trailing ->
+              atomically $
+                    Right . Right <$> stmRecvMessage
+                <|> pure (Right (Left state))
+
+        case result of
+          Right x@(Right (SomeMessage msg, _trailing')) -> do
+            traceWith tracer (TraceRecvMsg (AnyMessage msg))
+            return x
+          Right x@Left {} ->
+            return x
+          Left failure ->
+            throwIO failure
+
+    recvMessageSTM :: forall (st :: ps).
+                      SingI st
+                   => ActiveState st
+                   => StrictTVar m (Maybe (SomeAsync m))
+                   -> (ReflRelativeAgency (StateAgency st)
+                                           TheyHaveAgency
+                                          (Relative pr (StateAgency st)))
+                   -> DriverState ps pr st bytes failure (Maybe bytes) m
+                   -> m (STM m (SomeMessage st, Maybe bytes))
+    recvMessageSTM v _ (DecoderState decoder trailing) = mask_ $ do
+      hndl <- asyncWithUnmask $ \unmask ->
+                do labelThisThread "recv-stm"
+                   unmask (runDecoderWithChannel channel trailing decoder)
+                `finally`
+                atomically (writeTVar v Nothing)
+      atomically (writeTVar v (Just $! SomeAsync hndl))
+      return (do r <- waitSTM hndl
+                 case r of
+                   Left failure -> throwSTM failure
+                   Right result -> return result
+             )
+    recvMessageSTM v _ (DriverState trailing) = mask_ $ do
+      hndl <- asyncWithUnmask $ \unmask ->
+        do labelThisThread "recv-stm"
+           unmask (runDecoderWithChannel channel trailing =<< decode sing)
+        `finally`
+        atomically (writeTVar v Nothing)
+      atomically (writeTVar v (Just $! SomeAsync hndl))
+      return (do r <- waitSTM hndl
+                 writeTVar v Nothing
+                 case r of
+                   Left failure -> throwSTM failure
+                   Right result -> return result
+             )
+    recvMessageSTM _ _ (DriverStateSTM stmRecvMessage _) =
+      return stmRecvMessage
+
 
 
 -- | Run a peer with the given channel via the given codec.
@@ -166,16 +232,34 @@ driverSimple tracer Codec{encode, decode} channel@Channel{send} =
 --
 runPeer
   :: forall ps (st :: ps) pr pl failure bytes m a .
-     (MonadThrow m, Exception failure)
+     ( MonadAsync      m
+     , MonadMask       m
+     , MonadThrow (STM m)
+     , Exception failure
+     )
   => Tracer m (TraceSendRecv ps)
   -> Codec ps failure m bytes
   -> Channel m bytes
   -> Peer ps pr pl Empty st m a
   -> m (a, Maybe bytes)
-runPeer tracer codec channel peer =
+runPeer tracer codec channel peer = do
+    (driver, (v :: StrictTVar m (Maybe (SomeAsync m))))
+      <- driverSimple tracer codec channel
     runPeerWithDriver driver peer (startDState driver)
+      `catch` handleAsyncException v
   where
-    driver = driverSimple tracer codec channel
+    handleAsyncException :: StrictTVar m (Maybe (SomeAsync m))
+                         -> SomeAsyncException
+                         -> m (a, Maybe bytes)
+    handleAsyncException v e = do
+      (mbHndl :: Maybe (SomeAsync m))
+        <- (atomically :: forall x. STM m x -> m x)
+           (readTVar v :: STM m (Maybe (SomeAsync m)))
+      case mbHndl of
+        Nothing               -> throwIO e
+        Just (SomeAsync hndl) -> cancelWith hndl e
+                              >> throwIO e
+
 
 
 --
@@ -205,12 +289,10 @@ runDecoderWithChannel Channel{recv} = go
 tryRunDecoderWithChannel :: Monad m
                          => Channel m bytes
                          -> Maybe bytes
-                         -> DecodeStep bytes failure m a
+                         -> DecodeStep bytes failure m (SomeMessage st)
                          -> m (Either failure
-                                (Either ( DecodeStep bytes failure m a
-                                        , Maybe bytes
-                                        )
-                                        (a, Maybe bytes)))
+                                (Either (DriverState ps pr st bytes failure (Maybe bytes) m)
+                                        (SomeMessage st, Maybe bytes)))
 tryRunDecoderWithChannel Channel{tryRecv} = go
   where
     go _ (DecodeDone x trailing) = return (Right (Right (x, trailing)))
@@ -218,7 +300,7 @@ tryRunDecoderWithChannel Channel{tryRecv} = go
     go dstate@Nothing d@(DecodePartial k) = do
       r <- tryRecv
       case r of
-        Nothing -> return (Right (Left (d, dstate)))
+        Nothing -> return (Right (Left (DecoderState d dstate)))
         Just m  -> k m >>= go Nothing
     go (Just trailing) (DecodePartial k) = k (Just trailing) >>= go Nothing
 
@@ -233,8 +315,14 @@ data Role = Client | Server
 -- for example 'createConnectedChannels'.
 --
 runConnectedPeers :: forall ps pr pr' pl pl' st failure bytes m a b.
-                     (MonadSTM m, MonadAsync m, MonadCatch m,
-                      Exception failure, pr' ~ FlipAgency pr)
+                     ( MonadAsync      m
+                     , MonadMask       m
+                     , MonadSTM        m
+                     , MonadCatch      m
+                     , MonadThrow (STM m)
+                     , Exception failure
+                     , pr' ~ FlipAgency pr
+                     )
                   => m (Channel m bytes, Channel m bytes)
                   -> Tracer m (Role, TraceSendRecv ps)
                   -> Codec ps failure m bytes
