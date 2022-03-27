adc2af028b5483644102d9bc60706ba5323674c9
Marcin Szamotulski <coot@coot.me>
2022-03-27 17:09:01 +0200

    typed-protocols-examples: unbounded buffered channel
    A channel based on `TQueue`.  It is useful for testing pipelined
    protocols, where pipelining depth is not taken into account.

--- typed-protocols-examples/test/Network/TypedProtocol/PingPong/Tests.hs
+++ typed-protocols-examples/test/Network/TypedProtocol/PingPong/Tests.hs
@@ -303,7 +303,7 @@ prop_connect_pipelined5 choices (Positive omax) (NonNegative n) =
 
 -- | Run a non-pipelined client and server over a channel using a codec.
 --
-prop_channel :: (MonadSTM m, MonadAsync m, MonadCatch m)
+prop_channel :: (MonadLabelledSTM m, MonadTraceSTM m, MonadAsync m, MonadCatch m)
              => NonNegative Int
              -> m Bool
 prop_channel (NonNegative n) = do
