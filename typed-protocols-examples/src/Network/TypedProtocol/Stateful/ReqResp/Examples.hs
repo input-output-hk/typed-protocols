7fd4b120bc528c64ad19b6976bc1c9bcef3b605b
Marcin Szamotulski <coot@coot.me>
2022-06-30 10:16:30 +0200

    Added stateful ReqResp client

---
+++ typed-protocols-examples/src/Network/TypedProtocol/Stateful/ReqResp/Examples.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Network.TypedProtocol.Stateful.ReqResp.Examples
+  ( ReqRespStateCallbacks (..)
+  , reqRespClientMap
+  ) where
+
+import           Data.Kind (Type)
+
+import           Network.TypedProtocol.ReqResp.Type
+import           Network.TypedProtocol.Stateful.ReqResp.Client
+
+
+data ReqRespStateCallbacks (f :: ReqResp req resp -> Type) =
+    ReqRespStateCallbacks {
+        rrBusyToIdle :: f StBusy -> f StIdle
+      , rrBusyToBusy :: f StBusy -> f StBusy
+      , rrBusyToDone :: f StBusy -> f StDone
+      }
+
+reqRespClientMap
+  :: forall req resp f m.
+     Monad m
+  => ReqRespStateCallbacks f
+  -> f StBusy
+  -> [req]
+  -> ReqRespClient req resp f m ([resp], f StDone)
+reqRespClientMap ReqRespStateCallbacks
+                           { rrBusyToIdle
+                           , rrBusyToBusy
+                           , rrBusyToDone
+                           } = go []
+  where
+    go :: [resp]
+       -> f StBusy
+       -> [req]
+       -> ReqRespClient req resp f m ([resp], f StDone)
+    go resps f []         = SendMsgDone f' (pure (reverse resps, f'))
+      where
+        f' = rrBusyToDone f
+    go resps f (req:reqs) =
+      SendMsgReq f req $ \f' resp ->
+        ( return (go (resp:resps) (rrBusyToBusy f') reqs)
+        , rrBusyToIdle f'
+        )
+
