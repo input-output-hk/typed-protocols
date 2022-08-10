5ee65af2b1ebdabd8554b63d39abc6ef51bf2d06
Marcin Szamotulski <coot@coot.me>
2022-08-10 12:11:03 +0200

    stylish-haskell
    Fixed check-stylish on ubuntu.

--- scripts/check-stylish.sh
+++ scripts/check-stylish.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 export LC_ALL=C.UTF-8
 
-fd . './typed-protocols' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
-fd . './typed-protocols-cborg' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
-fd . './typed-protocols-examples' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
+[[ -x '/usr/bin/fd' ]] && FD="fd" ||  FD="fdfind"
+
+$FD . './typed-protocols' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
+$FD . './typed-protocols-cborg' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
+$FD . './typed-protocols-examples' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
